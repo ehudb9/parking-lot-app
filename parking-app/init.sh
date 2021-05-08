8fba22d80b40c06d2425d340a1ea30f16b88b7df
orzach-idc <83864848+orzach-idc@users.noreply.github.com>
2021-05-08 18:49:05 +0300

    Add files via upload

--- parking-app/init.sh
+++ parking-app/init.sh
@@ -5,7 +5,8 @@ sudo apt update
 sudo apt install python3-pip
 
 # installing aws cli
-sudo apt-get install awscli
+# sudo apt install awscli
+sudo pip3 install --upgrade awscli
 
 # installing aws sam 
 sudo pip3 install aws-sam-cli
