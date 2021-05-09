fe5dbae1a53d071e8ed94ee3e5cf58a3288586be
orzach-idc <83864848+orzach-idc@users.noreply.github.com>
2021-05-09 11:16:56 +0300

    Update exit.py

--- parking-app/src/exit.py
+++ parking-app/src/exit.py
@@ -19,31 +19,28 @@ def lambda_exit(event, context):
     # getting params from the request
     ticketId = event['queryStringParameters']["ticketId"]
 
-    try:
-        # getting the entry ticket data fro m the Parking-lot table in dynamodb
-        response = table.get_item(Key={'ID': ticketId})
+    # getting the entry ticket data fro m the Parking-lot table in dynamodb
+    response = table.get_item(Key={'ID': ticketId})
 
-        # convert entry time to datetime object
-        entryTimeStr = response['Item']['Entry_Time']
-        entryDateStr = response['Item']['Entry_Date']
-        entryTimeDateStr = entryDateStr + ' ' + entryTimeStr
-        entryTime = datetime.strptime(entryTimeDateStr, '%Y-%m-%d %H:%M:%S')
+    # convert entry time to datetime object
+    entryTimeStr = response['Item']['Entry_Time']
+    entryDateStr = response['Item']['Entry_Date']
+    entryTimeDateStr = entryDateStr + ' ' + entryTimeStr
+    entryTime = datetime.strptime(entryTimeDateStr, '%Y-%m-%d %H:%M:%S')
 
-        # calculating time difference
-        totalTime = exitTimeDate - entryTime
-        totalHours = totalTime.seconds // 3600
-        totalMin = (totalTime.seconds % 3600) // 60
-        charge = 2.5 * math.ceil(((totalTime.seconds / 60) / 15))
+    # calculating time difference
+    totalTime = exitTimeDate - entryTime
+    totalHours = totalTime.seconds // 3600
+    totalMin = (totalTime.seconds % 3600) // 60
+    charge = 2.5 * math.ceil(((totalTime.seconds / 60) / 15))
 
-        # preparing exit ticket
-        exitTicket['Plate Number'] = response['Item']['Plate_Number']
-        exitTicket['Total Parked Time'] = f"{totalHours} Hours and {totalMin} Minutes"
-        exitTicket['Parking Lot ID'] = response['Item']['Parking_Lot']
-        exitTicket['Charge'] = f"{charge} $"
+    # preparing exit ticket
+    exitTicket['Plate Number'] = response['Item']['Plate_Number']
+    exitTicket['Total Parked Time'] = f"{totalHours} Hours and {totalMin} Minutes"
+    exitTicket['Parking Lot ID'] = response['Item']['Parking_Lot']
+    exitTicket['Charge'] = f"{charge} $"
 
-        table.delete_item(Key={'ID': ticketId})
-        return {
-            'body': json.dumps(exitTicket)
+    table.delete_item(Key={'ID': ticketId})
+    return {
+        'body': json.dumps(exitTicket)
         }
-    except:
-        return {"Error": "Please enter a valid ticket id"}
